05f1de95a07adffd6b92594c53e3e840ed7ff335
najihmuhammad <najihmuham@yahoo.co.id>
2015-11-10 08:46:08 +0700

    Create lowongan.php

---
+++ lowongan.php
@@ -0,0 +1,72 @@
+<section class="success" id="lowongan">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Semua Lowongan</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row" font="Arial">
+                <table class="table table-condensed">
+				<tr>
+					<th>No</th>
+					<th>Perusahaan</th>
+					<th>Bagian </th>
+					<th>Detil Lowongan</th>
+					<th>Batas Pengiriman</th>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>PT ABCD</td>
+					<td>Administrasi</td>
+					<td>kualifikasi : <br>
+						Laki-laki maks 30 tahun <br>
+						Min D3, berpengalaman <br>
+						Jujur, rajin, teliti<br>
+						<a href="#">Selengkapnya </a>
+					<td> 5 November 2015 </td>
+					
+				<tr>
+					<td>2</td>
+					<td>PT CITRA BUANA TEKNOLOGI</td>
+					<td>Programmer</td>
+					<td>kualifikasi : Laki-laki<br>Min S1 IT, menguasai PHP CodeIgniter<br>
+					Laravel, bootsrap, <br><a href="#">Selengkapnya </a>
+					<td> 3 November 2015 </td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>CV BRAMASTHA ABIMANA</td>
+				<td>HRD</td>
+				<td>kualifikasi : <br>
+						Laki-laki maks 30 tahun <br>
+						Min D3, berpengalaman <br>
+						Jujur, rajin, teliti<br>
+						<a href="#">Selengkapnya </a> </td>
+				<td> 5 November 2015 </td>
+			
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>CV BRAMASTHA ABIMANA</td>
+				<td>HRD</td>
+				<td>kualifikasi : <br>
+						Laki-laki maks 30 tahun <br>
+						Min D3, berpengalaman <br>
+						Jujur, rajin, teliti<br>
+						<a href="#">Selengkapnya </a> </td>
+				<td> 5 November 2015 </td>
+			</tr>
+	
+		</table>
+			<nav>
+			  <ul class="pager">
+				<li class="previous"><a href="#"><span aria-hidden="true">&larr;</span> Back</a></li>
+				<li class="next"><a href="#">Next <span aria-hidden="true">&rarr;</span></a></li>
+			  </ul>
+			</nav>
+					</div>
+				</div>
+			</div>
+		</section>
+		
