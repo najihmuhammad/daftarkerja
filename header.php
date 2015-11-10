08d4136a63774c9aea5d3bbb3d4ceaa31dbf109c
najihmuhammad <najihmuham@yahoo.co.id>
2015-11-10 08:43:29 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+  <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="img/profile.png" alt="">
+                    <div class="intro-text">
+                        <span class="name">GetJobs</span>
+                        <hr class="star-light">
+                        <span class="skills">GetJobs Membantu anda menemukan pekerjaan impian!</span>
+						<div>
+						
+						</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
